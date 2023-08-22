17374ebcb6cd21a8cfd6ee9c4fb3639eabfb2c35
Dmitry Dronov <cskafan1911@gmail.com>
2023-08-23 00:43:56 +0300

    Добавлены атрибуты класса при инициализации

--- src/channel.py
+++ src/channel.py
@@ -1,4 +1,7 @@
 import os
+import json
+import isodate
+
 from googleapiclient.discovery import build
 
 
@@ -6,16 +9,33 @@ class Channel:
     """Класс для ютуб-канала"""
     # Переменная окружения
     API_KEY = os.getenv('YOUTUBE_API_KEY')
-
-    # Данные для работы с API youtube
     youtube = build('youtube', 'v3', developerKey=API_KEY)
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """
+        Экземпляр инициализирует id канала. Дальше все данные будут подтягиваться по API.
+        """
         self.channel_id = channel_id
+        self.channel = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = self.channel['items'][0]['snippet']['customUrl']
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+    # @classmethod
+    # def get_service(cls):
+    #     """
+    #     Возвращает объект для работы с YouTube API
+    #     """
+    #     return cls.youtube
 
-        print(channel)
+    # @staticmethod
+    # def get_info():
+    #     """
+    #     Возвращает информацию о канале
+    #     """
+    #     info_channel = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics')
+
+
