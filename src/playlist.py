544a26da8e47f46ecc8e0e009a49314c62a3d0bd
Dmitry Dronov <cskafan1911@gmail.com>
2023-09-06 21:02:14 +0300

    Написан метод show_best_video.

--- src/playlist.py
+++ src/playlist.py
@@ -20,19 +20,18 @@ class PlayList:
         self.playlist = self.youtube.playlistItems().list(playlistId=self.playlist_id,
                                                           part='contentDetails, snippet',
                                                           maxResults=50).execute()
-
+        self.video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist['items']]
         self.title = self.playlist['items'][0]['snippet']['title'].split(".")[0]
         self.url = "https://www.youtube.com/playlist?list=" + self.playlist_id
 
     @property
     def total_duration(self):
         """
-        Возвращает объект класса datetime.timedelta с суммарной длительность плейлиста
+        Объект класса datetime.timedelta с суммарной длительность плейлиста
         """
-        video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist['items']]
         total_duration = datetime.timedelta()
         video_response = self.youtube.videos().list(part='contentDetails,statistics',
-                                                    id=','.join(video_ids)
+                                                    id=','.join(self.video_ids)
                                                     ).execute()
 
         for video in video_response['items']:
@@ -42,3 +41,19 @@ class PlayList:
             total_duration += datetime.timedelta(hours=int(hour), minutes=int(minute), seconds=int(second))
 
         return total_duration
+
+    def show_best_video(self):
+        """
+        Возвращает ссылку на самое популярное видео из плейлиста (по количеству лайков)
+        """
+        likes_list = {}
+
+        for video_id in self.video_ids:
+            video = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=video_id).execute()
+            likes_list[('https://youtu.be/' + video['items'][0]['id'])] = (
+                int(video['items'][0]['statistics']['likeCount']))
+
+        values_ = list(likes_list.values())
+        keys_ = list(likes_list.keys())
+        return keys_[values_.index(max(values_))]
